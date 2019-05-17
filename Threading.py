0c7ca91763641f865a44bb3b35744352e1eb8853
sivannachum <43347661+sivannachum@users.noreply.github.com>
2019-05-17 15:12:28 -0400

    Threading

---
+++ Threading.py
@@ -0,0 +1,14 @@
+import threading
+
+
+class Messenger(threading.Thread):
+    def run(self):
+        for _ in range(10):
+            print(threading.currentThread().getName())
+
+
+x = Messenger(name='Send out messages')
+y = Messenger(name='Receive messages')
+# Because we're using threading, these can run concurrently
+x.start()
+y.start()
