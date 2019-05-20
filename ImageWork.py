fc31478c06ee923f62bfacfffe36069bd96eb6fe
sivannachum <43347661+sivannachum@users.noreply.github.com>
2019-05-20 09:44:21 -0400

    Image work

--- ImageWork.py
+++ ImageWork.py
@@ -1,7 +1,15 @@
 from PIL import Image
 
+# Open an image, show its size and format
 img = Image.open("79.jpg")
 print(img.size)
 print(img.format)
 
+# Show the image on an outside application
 img.show()
+
+# Crop the image
+# Starting x, starting y, ending x, ending y
+area = (100, 100, 300, 375)
+croppedImg = img.crop(area)
+croppedImg.show()
